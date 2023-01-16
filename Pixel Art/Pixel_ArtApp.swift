78a64f5d17a7c273eefbfd8f6661192d9ae0359a
Alexandar Hristov <ahristov@astea.net>
2023-01-16 19:11:44 +0200

    Initial XCode Files.

---
+++ Pixel Art/Pixel_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pixel_ArtApp.swift
+//  Pixel Art
+//
+//  Created by Alexandar Hristov on 16.01.23.
+//
+
+import SwiftUI
+
+@main
+struct Pixel_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

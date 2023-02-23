0f3fd061f9bf1c80a6a85dc86aa9debc2819d345
Alexandar Hristov <ahristov@astea.net>
2023-02-23 16:09:11 +0200

    Added app colors. RatingUIView and TopRatedUIView

---
+++ Pixel Art/RatingUIView.swift
@@ -0,0 +1,20 @@
+//
+//  RatingUIView.swift
+//  Pixel Art
+//
+//  Created by Alexandar Hristov on 23.02.23.
+//
+
+import SwiftUI
+
+struct RatingUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RatingUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingUIView()
+    }
+}
---
+++ Pixel Art/TopRatedUIView.swift
@@ -0,0 +1,20 @@
+//
+//  TopRatedUIView.swift
+//  Pixel Art
+//
+//  Created by Alexandar Hristov on 23.02.23.
+//
+
+import SwiftUI
+
+struct TopRatedUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopRatedUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopRatedUIView()
+    }
+}
